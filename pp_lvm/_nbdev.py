e20f62fbb650b78fd7ae94b134cff168abacbc30
Abhishek Sharma <shaabhishek@gmail.com>
2020-02-17 11:12:06 -0500

    working implementation

--- pp_lvm/_nbdev.py
+++ pp_lvm/_nbdev.py
@@ -2,9 +2,45 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"get_time_from_intervals": "01_simulations.ipynb"}
+index = {"device": "03_GP.ipynb",
+         "INDUCING_POINT_STRIDE": "00_core.ipynb",
+         "get_data": "00_core.ipynb",
+         "TrueParameters": "01_simulations.ipynb",
+         "transform_x": "01_simulations.ipynb",
+         "link_gamma": "01_simulations.ipynb",
+         "sample_using_logits": "01_simulations.ipynb",
+         "get_time_from_intervals": "01_simulations.ipynb",
+         "simulate_XZI_seq": "01_simulations.ipynb",
+         "simulate_XZI": "01_simulations.ipynb",
+         "simulate_H": "01_simulations.ipynb",
+         "simulate_Y": "01_simulations.ipynb",
+         "simulate_data": "01_simulations.ipynb",
+         "visualize": "01_simulations.ipynb",
+         "Encoder_h": "02_models.ipynb",
+         "Decoder_h": "02_models.ipynb",
+         "Encoder_x": "02_models.ipynb",
+         "Model_z": "02_models.ipynb",
+         "Decoder_i": "02_models.ipynb",
+         "Decoder_y": "02_models.ipynb",
+         "l_n_vectorized": "02_models.ipynb",
+         "l_n_vectorized_paper": "02_models.ipynb",
+         "psi_n_vectorized": "02_models.ipynb",
+         "compute_message": "02_models.ipynb",
+         "compute_message_paper": "02_models.ipynb",
+         "marginalize_z": "02_models.ipynb",
+         "marginalize_z_paper": "02_models.ipynb",
+         "add_jitter_covar": "03_GP.ipynb",
+         "get_covariance_matrix_from_RBFkernel": "03_GP.ipynb",
+         "get_covariance_matrix_from_RBFkernel_new": "03_GP.ipynb",
+         "plot_predictions": "03_GP.ipynb",
+         "ExactGPModelLayer": "03_GP.ipynb",
+         "Model_X": "03_GP.ipynb",
+         "inference_X": "03_GP.ipynb"}
 
-modules = ["simulations.py"]
+modules = ["core.py",
+           "simulations.py",
+           "models.py",
+           "GP.py"]
 
 doc_url = "https://abhishekshar.github.io/pp_lvm/"
 
